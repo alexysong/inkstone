b61145466efab1ff2085360aace820b28382d3b3
jadonylin <jadonlin2@gmail.com>
2024-06-24 15:21:08 +1000

    Add initial custom primitive for j0 function (unfinished)

--- inkstone/primitives.py
+++ inkstone/primitives.py
@@ -1,22 +1,44 @@
-from functools import partial
-import numpy.linalg as npla
-import numpy  as anp
+import numpy as np
+import scipy as sp
+import jax.numpy as jnp
+
+from jax import custom_vjp
 from autograd.extend import defvjp,primitive
 
-# Some formulas are from
-# "An extended collection of matrix derivative results
-#  for forward and reverse mode algorithmic differentiation"
-# by Mike Giles
-# https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf
-
-# transpose by swapping last two dimensions
-def T(x): return anp.swapaxes(x, -1, -2)
-
-_dot = partial(anp.einsum, '...ij,...jk->...ik')
-
-inv = primitive(anp.linalg.inv)
-def grad_inv(ans, x):
-    return lambda g: -_dot(_dot(T(ans), g), T(ans))
-defvjp(inv, grad_inv)
 
 
+# JAX ################################################################################################################################################################################################
+@custom_vjp
+def j0(x):
+    return sp.special.j0(x)
+
+def j0_fwd(x):
+    """
+    Second return value is tuple containing 'residual' data to be stored for use by back pass
+    """
+    return j0(x), (x,)
+
+def j0_bwd(res, g):
+    """
+    Parameters
+    ----------
+    res: residuals from fwd func (tuple)
+    g  : vector to backprop (scalar, for some nodes)
+
+    Return
+    ------
+    vjp: vector-Jacobian product of g with Jacobian (tuple)
+
+    NOTE: from custom_vjp docs, the output of bwd must be a tuple of length equal to the number 
+    of arguments of the primal function. Here, sinxsq has only 1 argument, so create a tuple of 
+    length 1 with (vjp,)
+
+    NOTE: you have to be careful that the inputs x and outputs vjp to the function have the 
+    same type, but cannot use type checking
+    """
+    x = res[0]
+    j0_jac = jnp.diag(sp.special.jvp(0, x, n=1))
+    vjp = g*j0_jac
+    return (vjp,)
+
+j0.defvjp(j0_fwd, j0_bwd)
