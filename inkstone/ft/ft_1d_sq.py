bdfd36b8b5d93137ae4d56e85fd933ccf40934a8
jadonylin <jadonlin2@gmail.com>
2024-06-26 09:38:30 +1000

    Fix function indentation

--- inkstone/ft/ft_1d_sq.py
+++ inkstone/ft/ft_1d_sq.py
@@ -3,36 +3,36 @@ from inkstone.GenericBackend import genericBackend as gb
 
 
 def ft_1d_sq(width, ks, center=0. , gb=gb):
+   """
+   Calculate the Fourier transform of a 1d square function.
 
-    """
-    Calculate the Fourier transform of a 1d square function.
+   The square function looks like this:
 
-    The square function looks like this:
+         __________
+   ______|    1     |____0____
 
-           __________
-    ______|    1     |____0____
+   Parameters
+   ----------
+   center      :   float
+                  center of the square
+   width       :   float
+                  width of the square
+   ks          :   list
+                  list of k points
 
-    Parameters
-    ----------
-    center      :   float
-                    center of the square
-    width       :   float
-                    width of the square
-    ks          :   list
-                    list of k points
+   Returns
+   -------
+   s           :   list[complex]
+                  1D array, Fourier series coefficients at the corresponding points
+   """
+   if width <= 0:
+      raise ValueError("No zero or negative width")
 
-    Returns
-    -------
-    s           :   list[complex]
-                    1D array, Fourier series coefficients at the corresponding points
-    """
-    if width <= 0:
-       raise ValueError("No zero or negative width")
+   ksa = gb.parseData(ks)
 
-    ksa = gb.parseData(ks)
+   s = gb.exp(-1j * center * ksa) * width * gb.sinc(ksa * width / 2. / gb.pi)
+   # note numpy sinc(x) definition is sin(pi x) / (pi x)
 
-    s = gb.exp(-1j * center * ksa) * width * gb.sinc(ksa * width / 2. / gb.pi)
-    # note numpy sinc(x) definition is sin(pi x) / (pi x)
-
-    return s.tolist()
+   # return s
+   return s.tolist()
 
