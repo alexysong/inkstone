564d810a5a524feb28ccb656dd1b31c199b5b4d6
jadonylin <jadonlin2@gmail.com>
2024-07-01 15:54:36 +1000

    Add comments to debug_derivatives.py

--- inkstone/tests/test_inkstone/debug_derivatives.py
+++ inkstone/tests/test_inkstone/debug_derivatives.py
@@ -1,3 +1,7 @@
+"""
+File for debugging inkstone_objective_functions.py functions
+"""
+
 import numpy as np
 import jax.numpy as jnp
 import jax
@@ -7,85 +11,102 @@ from jax import grad as jax_grad
 from inkstone import GenericBackend
 from inkstone.simulator import Inkstone as NewInkstone # rename to NewInkstone so switchTo works correctly
 
-def reflection_1d(d,w,f,permittivity,num_g=30,backend="jax"):
-    GenericBackend.switchTo(backend)
-    s = NewInkstone()
-    s.frequency = f
-    s.lattice = 1
-    s.num_g = num_g
+import matplotlib.pyplot as plt
 
-    s.AddMaterial(name='di', epsilon=permittivity)
 
-    s.AddLayer(name='in', thickness=0, material_background='vacuum')
-    s.AddLayer(name='slab', thickness=d, material_background='di')
-    s.AddLayerCopy(name='out', original_layer='in', thickness=0)
 
-    s.AddPattern1D(layer='slab', pattern_name='box', material='vacuum', width=w, center=0.5)
-
-    s.SetExcitation(theta=0, phi=0, s_amplitude=1, p_amplitude=0)
-
-    i, r = s.GetPowerFlux('in')
-    return -r / i
-
-def abs_Ey_halfway(d,w,f,permittivity,num_g=30,backend="jax"):
-    """
-    Returns abs(Ey) at (x,y,z)=(0,0,d/2)
-    """
-    GenericBackend.switchTo(backend)
-    s = NewInkstone()
-    s.frequency = f
-    s.lattice = 1
-    s.num_g = num_g
-
-    s.AddMaterial(name='di', epsilon=permittivity)
-
-    s.AddLayer(name='in', thickness=0, material_background='vacuum')
-    s.AddLayer(name='slab', thickness=d, material_background='di')
-    s.AddLayerCopy(name='out', original_layer='in', thickness=0)
-
-    s.AddPattern1D(layer='slab', pattern_name='box', material='vacuum', width=w, center=0.5)
-
-    s.SetExcitation(theta=0, phi=0, s_amplitude=1, p_amplitude=0)
-
-    Ex, Ey, Ez, Hx, Hy, Hz = s.GetFields(x=0,y=0,z=d/2)
-    
-    return GenericBackend.genericBackend.abs(Ey[0][0][0])
-
+# GLOBAL SETTINGS ################################################################################################################################################
 eps = 1e-6
 abs_tol = 1e-6
 rel_tol = 1e-5
 
-params_1d = (1.0, 0.4, 0.2, 12.)
-d,w,f,permittivity = params_1d
 
-# reflection_1d_grad0 = jax_grad(reflection_1d,0)
-# reflection_1d_grad1 = jax_grad(reflection_1d,1)
-# reflection_1d_grad2 = jax_grad(reflection_1d,2)
-# reflection_1d_grad3 = jax_grad(reflection_1d,3)
-# abs_Ey_halfway_grad0 = jax_grad(abs_Ey_halfway,0)
-# abs_Ey_halfway_grad1 = jax_grad(abs_Ey_halfway,1)
-# abs_Ey_halfway_grad2 = jax_grad(abs_Ey_halfway,2)
-# abs_Ey_halfway_grad3 = jax_grad(abs_Ey_halfway,3)
 
-# params = params_1d[1:]
-# FD_grad = (reflection_1d(d+eps/2,*params,num_g=30,backend="jax") - reflection_1d(d-eps/2,*params,num_g=30,backend="jax"))/eps
-# AD_grad = reflection_1d_grad0(d,*params,backend="jax")
-# FD_grad = (abs_Ey_halfway(d+eps/2,*params,num_g=30,backend="jax") - abs_Ey_halfway(d-eps/2,*params,num_g=30,backend="jax"))/eps
-# AD_grad = abs_Ey_halfway_grad0(d,*params,backend="jax")
+# 1D 1 LAYER ################################################################################################################################################
+### FUNCTIONS ###
+def simulation_1layer_1d(d,w,f,p,num_g=30,backend="jax"):
+    GenericBackend.switchTo(backend)
+    s = NewInkstone()
+    s.frequency = f
+    s.lattice = 1
+    s.num_g = num_g
 
-# params = params_1d[2:]
-# FD_grad = (reflection_1d(d,w+eps/2,*params,num_g=30,backend="jax") - reflection_1d(d,w-eps/2,*params,num_g=30,backend="jax"))/eps
-# AD_grad = reflection_1d_grad1(d,w,*params,num_g=30,backend="jax")
+    s.AddMaterial(name='di', epsilon=p)
 
-# FD_grad = (reflection_1d(d,w,f+eps/2,permittivity,num_g=30,backend="jax") - reflection_1d(d,w,f-eps/2,permittivity,num_g=30,backend="jax"))/eps
-# AD_grad = reflection_1d_grad2(d,w,f,permittivity,num_g=30,backend="jax")
+    s.AddLayer(name='in', thickness=0, material_background='vacuum')
+    s.AddLayer(name='slab', thickness=d, material_background='di')
+    s.AddLayerCopy(name='out', original_layer='in', thickness=0)
 
-# params = params_1d[:3]
-# FD_grad = (reflection_1d(*params,permittivity+eps/2,num_g=30,backend="jax") - reflection_1d(*params,permittivity-eps/2,num_g=30,backend="jax"))/eps
-# AD_grad = reflection_1d_grad3(*params,permittivity,num_g=30,backend="jax")
+    s.AddPattern1D(layer='slab', pattern_name='box', material='vacuum', width=w, center=0.5)
 
-# assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
+    s.SetExcitation(theta=0, phi=0, s_amplitude=1, p_amplitude=0)
 
+    return s
+
+def reflection_1layer_1d(d,w,f,p,num_g=30,backend="jax"):
+    """
+    Return m=0 reflection coefficient without diffraction
+    """
+    s = simulation_1layer_1d(d,w,f,p,num_g,backend)
+    i, r = s.GetPowerFlux('in')
+    return -r / i
+
+def fields_1layer_1d(d,w,f,p,z,num_g=30,backend="jax"):
+    """
+    Returns fields at x=0, y=0, z=z
+    """
+    s = simulation_1layer_1d(d,w,f,p,num_g,backend)
+    return s.GetFields(x=0,y=0,z=z)
+
+def abs_Ey_1layer_1d(d,w,f,p,z,num_g=30,backend="jax"):
+    """
+    Returns abs(Ey) at (x,y,z)=(0,0,z)
+    """
+    GenericBackend.switchTo(backend)
+    Ex, Ey, Ez, Hx, Hy, Hz = fields_1layer_1d(d,w,f,p,z,num_g,backend)
+    # return jnp.real(Ey[0][0][0])
+    return GenericBackend.genericBackend.abs(Ey[0][0][0])
+
+
+
+### PARAMETERS ###
+def params_1layer_1d():
+    d = 1.
+    w = 0.4
+    f = 0.2
+    p = 12.
+    z = 1/2
+    return [d,w,f,p,z]
+
+
+
+### TEST FD VS AD ###
+# Verified real(Ey) vs z: expect sinusoidal Ey inside grating, perfect Ey sines outside the grating
+# Verified |Ey| vs z: expect sinusoidal |Ey| inside grating, constant |Ey| in output region and sinusoidal |Ey| in incidence region
+# Sinusoidal Ey inside grating comes from the Fourier (sine) waves mixing together
+# |Ey| is sinusoidal in incidence region because the incident wave mixes with the reflected wave
+# params_left = params_1layer_1d()[:4]
+# def Ey(z):
+#     return abs_Ey_1layer_1d(*params_left,z,num_g=30,backend="jax")
+# z_positions = jnp.linspace(-5.,5.,200, dtype=jnp.float64) 
+# Eys = jnp.array([Ey(z) for z in z_positions])
+# plt.plot(z_positions, Eys, '-k')
+# plt.show()
+
+# |Ey| vs d: for z fixed (or proportional to d), |Ey| is periodic in d
+# Likely comes from the exp(iq(d-z)) dependence
+# params_right = params_1layer_1d()[1:]
+# def Ey(d):
+#     return abs_Ey_1layer_1d(d,*params_right,num_g=30,backend="jax")
+# grating_thicknesses = jnp.linspace(1,10.,200, dtype=jnp.float64) 
+# Eys = jnp.array([Ey(d) for d in grating_thicknesses])
+# plt.plot(grating_thicknesses, Eys, '-k')
+# plt.show()
+
+
+
+# 1D 2 LAYER ################################################################################################################################################
+### FUNCTIONS ###
 def simulation_2layer_1d(d1,d2,w1,w2,p1,p2,f,theta,num_g=30,backend="jax"):
     GenericBackend.switchTo(backend)
     s = NewInkstone()
@@ -136,7 +157,8 @@ def abs_Ey_2layer_1d(d1,d2,w1,w2,p1,p2,f,theta,z,num_g=30,backend="jax"):
     return GenericBackend.genericBackend.abs(Ey[0][0][0])
 
 
-# 2 layer 1D
+
+### PARAMETERS ###
 def params_2layer_1d():
     d1 = 0.5
     d2 = 10.
@@ -146,9 +168,13 @@ def params_2layer_1d():
     p2 = 0.05
     f = 10.
     theta = 25. # degrees
-    z = d1
+    z = 1.05*d1
     return [d1,d2,w1,w2,p1,p2,f,theta,z]
 
+
+
+### TESTING FD VS AD ###
+## Ey vs z position ##
 autodiffgrad = jax_grad(abs_Ey_2layer_1d, 8)
 
 params_left = params_2layer_1d()[:8]
@@ -166,6 +192,7 @@ z = 0.5
 #     assert np.allclose(AD_grad, FD_grad, rtol=1000*rel_tol, atol=abs_tol)
 
 
+## Reflection vs theta ##
 autodiffgrad = jax_grad(reflection_2layer_1d, 7)
 angles = jnp.linspace(-30.,30.,5, dtype=jnp.float64)
 params_left = params_2layer_1d()[:7] 
@@ -175,7 +202,6 @@ theta = 30.
 # def refl(th,order):
 #     return reflection_2layer_1d(*params_left,th,order=order)
 
-# import matplotlib.pyplot as plt
 # thetas = jnp.linspace(-30.,30.,100, dtype=jnp.float64)
 # print(params_left)
 # rNeg1 = jnp.array([refl(th,order=-1) for th in thetas])
@@ -189,10 +215,36 @@ theta = 30.
 # plt.legend()
 # plt.show()
 
-for th in angles:
-    FD_grad = (reflection_2layer_1d(*params_left,th+eps/2,order=-1,num_g=30,backend="jax") 
-                - reflection_2layer_1d(*params_left,th-eps/2,order=-1,num_g=30,backend="jax"))/eps
-    AD_grad = autodiffgrad(*params_left,th,order=-1,num_g=30,backend="jax")
-    assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
+# for th in angles:
+#     FD_grad = (reflection_2layer_1d(*params_left,th+eps/2,order=-1,num_g=30,backend="jax") 
+#                 - reflection_2layer_1d(*params_left,th-eps/2,order=-1,num_g=30,backend="jax"))/eps
+#     AD_grad = autodiffgrad(*params_left,th,order=-1,num_g=30,backend="jax")
+#     assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
 # nan -1.777129491620233
+
+
+## Ey vs layer 2 thickness ##
+# autodiffgrad = jax_grad(abs_Ey_2layer_1d, 1)
+# grating_thicknesses = jnp.linspace(0.1,100,5, dtype=jnp.float64) # FD diverges at d2 = 0.1
+grating_thicknesses = jnp.linspace(1,100,5, dtype=jnp.float64) 
+params_left = params_2layer_1d()[:1] 
+params_right = params_2layer_1d()[2:]
+eps = 1e-7
+num_g = 30
+
+def Ey(d):
+    return abs_Ey_2layer_1d(*params_left,d,*params_right,num_g=num_g,backend="jax")
+
+grating_thicknesses = jnp.linspace(1,10,200, dtype=jnp.float64) 
+Eys = jnp.array([Ey(d) for d in grating_thicknesses])
+plt.plot(grating_thicknesses, Eys, '-k')
+plt.show()
+
+# for d2 in grating_thicknesses:
+#     Ey_plus = abs_Ey_2layer_1d(*params_left,d2+eps/2,*params_right,num_g=num_g,backend="jax")
+#     Ey_minus = abs_Ey_2layer_1d(*params_left,d2-eps/2,*params_right,num_g=num_g,backend="jax")
+#     FD_grad = (Ey_plus - Ey_minus)/eps
+#     AD_grad = autodiffgrad(*params_left,d2,*params_right,num_g=num_g,backend="jax")
+#     print(f"Ey(d2+eps/2) = {Ey_plus}\nEy(d2-eps/2) = {Ey_minus}\nFD_grad = {FD_grad}\nAD_grad = {AD_grad}")
+#     assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
