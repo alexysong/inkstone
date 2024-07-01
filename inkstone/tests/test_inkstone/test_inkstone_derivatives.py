b25b9489467f0f1b651b49f47e7853ce0608ab97
jadonylin <jadonlin2@gmail.com>
2024-07-01 19:30:41 +1000

    Use fixture function for 1d 1 layer tests for consistency
    Add docstrings. Add possible explanations for currently failing tests

--- inkstone/tests/test_inkstone/test_inkstone_derivatives.py
+++ inkstone/tests/test_inkstone/test_inkstone_derivatives.py
@@ -1,3 +1,8 @@
+"""
+Testing automatic differentiation of inkstone-based objective functions matches finite difference derivatives.
+
+"""
+
 import numpy as np
 import jax.numpy as jnp
 from jax import grad as jax_grad
@@ -18,6 +23,7 @@ eps = 1e-6
 
 
 # SETUP ################################################################################################
+## PARAMETERS ##
 @pytest.fixture
 def params_1layer_1d():
     d = 1.
@@ -37,9 +43,8 @@ def params_2layer_1d():
     p2 = 0.05
     f = 10.
     theta = 25. # degrees
-    z = d1
+    z = 1.05*d1
     return [d1,d2,w1,w2,p1,p2,f,theta,z]
-
 
 @pytest.fixture
 def params_1layer_2d():
@@ -53,6 +58,21 @@ def params_1layer_2d():
     z = d/2
     return [d,r,f,p,theta,phi,x,z]
 
+
+## GRADIENT FUNCTIONS ##
+@pytest.fixture
+def reflection_1layer_1d_grad():
+    def _reflection_1layer_1d_grad(derivative_argnum=0):
+        return jax_grad(reflection_1layer_1d, derivative_argnum)
+
+    return _reflection_1layer_1d_grad
+
+@pytest.fixture
+def abs_Ey_1layer_1d_grad():
+    def _abs_Ey_1layer_1d_grad(derivative_argnum=0):
+        return jax_grad(abs_Ey_1layer_1d, derivative_argnum)
+
+    return _abs_Ey_1layer_1d_grad
 
 @pytest.fixture
 def reflection_2layer_1d_grad():
@@ -88,121 +108,119 @@ def abs_Ey_1layer_2d_grad():
 # JAX ################################################################################################
 ## 1D SIMULATION ##
 # reflection_1layer_1d #
-reflection_1layer_1d_grad0 = jax_grad(reflection_1layer_1d,0)
-reflection_1layer_1d_grad1 = jax_grad(reflection_1layer_1d,1)
-reflection_1layer_1d_grad2 = jax_grad(reflection_1layer_1d,2)
-reflection_1layer_1d_grad3 = jax_grad(reflection_1layer_1d,3)
-
-def test_reflection_1layer_1d_grad0(params_1layer_1d):
+def test_reflection_1layer_1d_grad_d(params_1layer_1d, reflection_1layer_1d_grad):
     """
     Test jax gradient of reflection_1layer_1d wrt grating thickness matches finite difference
     """
+    autodiffgrad = reflection_1layer_1d_grad(derivative_argnum=0)
     grating_thicknesses = jnp.linspace(0.1,100,5, dtype=jnp.float64)
     params = params_1layer_1d[1:4]
     for d in grating_thicknesses:
         FD_grad = (reflection_1layer_1d(d+eps/2,*params,num_g=30,backend="jax") - reflection_1layer_1d(d-eps/2,*params,num_g=30,backend="jax"))/eps
-        AD_grad = reflection_1layer_1d_grad0(d,*params,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,*params,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
     
-def test_reflection_1layer_1d_grad1(params_1layer_1d):
+def test_reflection_1layer_1d_grad_w(params_1layer_1d, reflection_1layer_1d_grad):
     """
     Test jax gradient of reflection_1layer_1d wrt grating resonator width matches finite difference
     """
+    autodiffgrad = reflection_1layer_1d_grad(derivative_argnum=1)
     grating_widths = jnp.linspace(0.01,0.99,5, dtype=jnp.float64)
     d = params_1layer_1d[0]
     params = params_1layer_1d[2:4]
     for w in grating_widths:
         FD_grad = (reflection_1layer_1d(d,w+eps/2,*params,num_g=30,backend="jax") - reflection_1layer_1d(d,w-eps/2,*params,num_g=30,backend="jax"))/eps
-        AD_grad = reflection_1layer_1d_grad1(d,w,*params,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,w,*params,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
-def test_reflection_1layer_1d_grad2(params_1layer_1d):
+def test_reflection_1layer_1d_grad_f(params_1layer_1d, reflection_1layer_1d_grad):
     """
     Test jax gradient of reflection_1layer_1d wrt frequency matches finite difference
     """
+    autodiffgrad = reflection_1layer_1d_grad(derivative_argnum=2)
     frequencies = jnp.linspace(0.01,0.99,5, dtype=jnp.float64)
     d,w = params_1layer_1d[:2]
     p = params_1layer_1d[3]
     for f in frequencies:
         FD_grad = (reflection_1layer_1d(d,w,f+eps/2,p,num_g=30,backend="jax") - reflection_1layer_1d(d,w,f-eps/2,p,num_g=30,backend="jax"))/eps
-        AD_grad = reflection_1layer_1d_grad2(d,w,f,p,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,w,f,p,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
-def test_reflection_1layer_1d_grad3(params_1layer_1d):
+def test_reflection_1layer_1d_grad_p(params_1layer_1d, reflection_1layer_1d_grad):
     """
     Test jax gradient of reflection_1layer_1d wrt grating permittivity matches finite difference
     """
+    autodiffgrad = reflection_1layer_1d_grad(derivative_argnum=3)
     permittivities = jnp.logspace(-2,2,5, dtype=jnp.float64)
     params = params_1layer_1d[:3]
     for p in permittivities:
         FD_grad = (reflection_1layer_1d(*params,p+eps/2,num_g=30,backend="jax") - reflection_1layer_1d(*params,p-eps/2,num_g=30,backend="jax"))/eps
-        AD_grad = reflection_1layer_1d_grad3(*params,p,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(*params,p,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
 
 # abs_Ey_1layer_1d #
-abs_Ey_1layer_1d_grad0 = jax_grad(abs_Ey_1layer_1d,0)
-abs_Ey_1layer_1d_grad1 = jax_grad(abs_Ey_1layer_1d,1)
-abs_Ey_1layer_1d_grad2 = jax_grad(abs_Ey_1layer_1d,2)
-abs_Ey_1layer_1d_grad3 = jax_grad(abs_Ey_1layer_1d,3)
-abs_Ey_1layer_1d_grad4 = jax_grad(abs_Ey_1layer_1d,4)
-
-def test_abs_Ey_1layer_1d_grad0(params_1layer_1d):
+def test_abs_Ey_1layer_1d_grad_d(params_1layer_1d, abs_Ey_1layer_1d_grad):
     """
     Test jax gradient of abs_Ey_1layer_1d wrt grating thickness matches finite difference
     """
+    autodiffgrad = abs_Ey_1layer_1d_grad(derivative_argnum=0)
     grating_thicknesses = jnp.linspace(0.1,100,5, dtype=jnp.float64)
     params = params_1layer_1d[1:]
     for d in grating_thicknesses:
         FD_grad = (abs_Ey_1layer_1d(d+eps/2,*params,num_g=30,backend="jax") - abs_Ey_1layer_1d(d-eps/2,*params,num_g=30,backend="jax"))/eps
-        AD_grad = abs_Ey_1layer_1d_grad0(d,*params,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,*params,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
     
-def test_abs_Ey_1layer_1d_grad1(params_1layer_1d):
+def test_abs_Ey_1layer_1d_grad_w(params_1layer_1d, abs_Ey_1layer_1d_grad):
     """
     Test jax gradient of abs_Ey_1layer_1d wrt grating resonator width matches finite difference
     """
+    autodiffgrad = abs_Ey_1layer_1d_grad(derivative_argnum=1)
     grating_widths = jnp.linspace(0.01,0.99,5, dtype=jnp.float64)
     d = params_1layer_1d[0]
     params = params_1layer_1d[2:]
     for w in grating_widths:
         FD_grad = (abs_Ey_1layer_1d(d,w+eps/2,*params,num_g=30,backend="jax") - abs_Ey_1layer_1d(d,w-eps/2,*params,num_g=30,backend="jax"))/eps
-        AD_grad = abs_Ey_1layer_1d_grad1(d,w,*params,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,w,*params,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
-def test_abs_Ey_1layer_1d_grad2(params_1layer_1d):
+def test_abs_Ey_1layer_1d_grad_f(params_1layer_1d, abs_Ey_1layer_1d_grad):
     """
     Test jax gradient of abs_Ey_1layer_1d wrt frequency matches finite difference
     """
+    autodiffgrad = abs_Ey_1layer_1d_grad(derivative_argnum=2)
     frequencies = jnp.linspace(0.01,0.99,5, dtype=jnp.float64)
     d,w = params_1layer_1d[:2]
     p, z = params_1layer_1d[3:5]
     for f in frequencies:
         FD_grad = (abs_Ey_1layer_1d(d,w,f+eps/2,p,z,num_g=30,backend="jax") - abs_Ey_1layer_1d(d,w,f-eps/2,p,z,num_g=30,backend="jax"))/eps
-        AD_grad = abs_Ey_1layer_1d_grad2(d,w,f,p,z,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(d,w,f,p,z,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
-def test_abs_Ey_1layer_1d_grad3(params_1layer_1d):
+def test_abs_Ey_1layer_1d_grad_p(params_1layer_1d, abs_Ey_1layer_1d_grad):
     """
     Test jax gradient of abs_Ey_1layer_1d wrt grating permittivity matches finite difference
     """
+    autodiffgrad = abs_Ey_1layer_1d_grad(derivative_argnum=3)
     permittivities = jnp.logspace(-2,2,5, dtype=jnp.float64)
     params = params_1layer_1d[:3]
     z = params_1layer_1d[4]
     for p in permittivities:
         FD_grad = (abs_Ey_1layer_1d(*params,p+eps/2,z,num_g=30,backend="jax") - abs_Ey_1layer_1d(*params,p-eps/2,z,num_g=30,backend="jax"))/eps
-        AD_grad = abs_Ey_1layer_1d_grad3(*params,p,z,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(*params,p,z,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
-def test_abs_Ey_1layer_1d_grad4(params_1layer_1d):
+def test_abs_Ey_1layer_1d_grad_z(params_1layer_1d, abs_Ey_1layer_1d_grad):
     """
     Test jax gradient of abs_Ey_1layer_1d wrt z position matches finite difference
     """
+    autodiffgrad = abs_Ey_1layer_1d_grad(derivative_argnum=4)
     params = params_1layer_1d[:4]
     z_positions = jnp.linspace(-1,1.05*params[0],5, dtype=jnp.float64)
     for z in z_positions:
         FD_grad = (abs_Ey_1layer_1d(*params,z+eps/2,num_g=30,backend="jax") - abs_Ey_1layer_1d(*params,z-eps/2,num_g=30,backend="jax"))/eps
-        AD_grad = abs_Ey_1layer_1d_grad4(*params,z,num_g=30,backend="jax")
+        AD_grad = autodiffgrad(*params,z,num_g=30,backend="jax")
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
 
@@ -251,6 +269,23 @@ def test_reflection_2layer_1d_grad_p2(params_2layer_1d, reflection_2layer_1d_gra
 def test_reflection_2layer_1d_grad_theta(params_2layer_1d, reflection_2layer_1d_grad):
     """
     Test jax gradient of reflection_2layer_1d wrt incident angle matches finite difference
+
+    AssertionError: 
+        AD_grad = nan, FD_grad = 0.1961769746719466
+    JL's progress: 
+        The error seems to come from JAX being unable to take the jnp.sqrt of a TracedConcreteArray(0.) in params.py
+        Specifically, where the argument of the square root can be zero:
+            _calc_q0(): q0 = self.gb.sqrt(q02)
+            _calc_angles(): sthe = self.gb.sqrt(1 - cthe**2 + 0j) 
+        JAX has no issue with jnp.sqrt(0.) = 0 or jnp.sqrt(0.+0j) = 0, but the square root of the Traced array creates NaN
+        Attempted fix: 
+            Converting NaNs to zero, e.g. sthe = self.gb.indexAssign(sthe, self.gb.isnan(sthe), 0)
+            This does not solve the issue, since the final gradient is still NaN (it is possible that JAX cannot differentiate gb.isnan)
+        Alternative attempted fix: 
+            I also tried converting all instances of zero in q02 and 1-cthe**2+0j to a small positive number 
+            1.e-15+0j before taking the square root. Gives a non-NaN gradient, but final AD_grad is inconsistent with FD_grad.
+            I am not sure if this is allowed in the physics, or computationally valid since the sqrt of a small number becomes larger
+        A better fix would not allow modifying values.
     """
     autodiffgrad = reflection_2layer_1d_grad(derivative_argnum=7)
     angles = jnp.linspace(-30.,30.,5, dtype=jnp.float64)
@@ -260,7 +295,7 @@ def test_reflection_2layer_1d_grad_theta(params_2layer_1d, reflection_2layer_1d_
         FD_grad = (reflection_2layer_1d(*params_left,th+eps/2,order=-1,num_g=30,backend="jax") 
                    - reflection_2layer_1d(*params_left,th-eps/2,order=-1,num_g=30,backend="jax"))/eps
         AD_grad = autodiffgrad(*params_left,th,order=-1,num_g=30,backend="jax")
-        print(AD_grad,FD_grad) 
+        # print(AD_grad,FD_grad) 
         assert np.allclose(AD_grad, FD_grad, rtol=rel_tol, atol=abs_tol)
 
 
@@ -268,13 +303,19 @@ def test_reflection_2layer_1d_grad_theta(params_2layer_1d, reflection_2layer_1d_
 def test_abs_Ey_2layer_1d_grad_d2(params_2layer_1d, abs_Ey_2layer_1d_grad):
     """
     Test jax gradient of abs_Ey_2layer_1d wrt second layer grating thickness matches finite difference
+
+    AssertionError:
+        Test case fails likely because |Ey| does not seem to be continuous with respect to grating thickness.
+        In the 1D, 1 layer case, the fields were periodic in d because of the exp(iq(d-z)) Fourier field dependence
+        Why is |Ey| no longer continuous (let alone periodic) in d2 in the 1D, 2 layer case?
+            Can check discontinuity by plotting |Ey| vs d2 for a small d2 range and increasing the number of plot points.
     """
     autodiffgrad = abs_Ey_2layer_1d_grad(derivative_argnum=1)
     # grating_thicknesses = jnp.linspace(0.1,100,5, dtype=jnp.float64) # FD diverges at d2 = 0.1
     grating_thicknesses = jnp.linspace(1,100,5, dtype=jnp.float64)
     params_left = params_2layer_1d[:1] 
     params_right = params_2layer_1d[2:]
-    eps = 1e-7
+    eps = 1e-6
     for d2 in grating_thicknesses:
         FD_grad = (abs_Ey_2layer_1d(*params_left,d2+eps/2,*params_right,num_g=30,backend="jax") 
                    - abs_Ey_2layer_1d(*params_left,d2-eps/2,*params_right,num_g=30,backend="jax"))/eps
